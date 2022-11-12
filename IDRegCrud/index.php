05d6a04f8d0c0b5db0213c279eb145c02dd36b90
mnl0609 <marinelarevalo05@gmail.com>
2022-11-12 09:15:53 +0800

    Initial

---
+++ IDRegCrud/index.php
@@ -0,0 +1,17 @@
+<?php
+include 'functions.php';
+// Your PHP code here.
+
+// Home Page template below.
+?>
+
+<?=template_header('Home')?>
+
+<div class="content">
+	<h2>WELCOME!</h2>
+	<p>We welcome you to the <b>UNIVERSITY OF THE CORDILLERAS!</b></p>
+	
+	<p>Complete the process and get your Unique Identification Number as a UCian!</p>
+</div>
+
+<?=template_footer()?>
